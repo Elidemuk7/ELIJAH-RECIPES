868a10da871657ca90ba4fdca6e51a321bffd77a
Elijah Mukoro <elijahmukoro7@gmail.com>
2024-04-18 23:32:08 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import './App.scss';
-// react router dom
+import './App.scss'; // Importing CSS file for styling the App component.
+
+// Importing necessary components from React Router DOM for routing.
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
-// pages
+
+// Importing page components from the pages folder.
 import { Home, MealDetails, Error, Category } from "./pages/index";
-// components
+
+// Importing Header and Sidebar components.
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 
+// Functional component named App.
 function App() {
   return (
+    // BrowserRouter component for handling routing.
     <BrowserRouter>
+      {/* Header component */}
       <Header />
+      {/* Sidebar component */}
       <Sidebar />
+      {/* Routes for defining page routes */}
       <Routes>
+        {/* Route for home page */}
         <Route path = "/" element = {<Home />} />
+        {/* Route for meal details page */}
         <Route path = "/meal/:id" element = {<MealDetails />} />
+        {/* Route for category page */}
         <Route path = "/meal/category/:name" element = {<Category />} />
+        {/* Route for handling unknown paths */}
         <Route path  = "*" element = {<Error />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default App; // Exporting the App component.
+
